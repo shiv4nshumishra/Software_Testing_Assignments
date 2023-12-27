da8eda8c019b29b971400818014873d363e851a5
Shivanshu Mishra <shivanshumishra.email@gmail.com>
2023-12-27 20:34:41 +0530

    Facebook&Google Project added.

---
+++ PHASE - 2/browserAutomationTesting/src/main/java/com/shivanshu/SeleniumWithJUnit/App.java
@@ -0,0 +1,13 @@
+package com.shivanshu.SeleniumWithJUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
