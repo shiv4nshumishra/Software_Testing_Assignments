cc4f5116a938de459712ef4647a4be34038a4aa2
Shivanshu Mishra <shivanshumishra.email@gmail.com>
2023-12-27 18:19:54 +0530

    added some missed files....

---
+++ PHASE - 1/com.shivanshu.selenium_project/src/main/java/com/simplilearn/seleniumtest/LauchFireFoxTest.java
@@ -0,0 +1,29 @@
+package com.simplilearn.seleniumtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LauchFireFoxTest {
+
+	public static void main(String[] args) {
+		
+		// step1: formulate a test domain url & driver path
+		String siteUrl = "https://www.google.com/";
+		String driverPath = "drivers/windows/geckodriver.exe";
+		
+		// step2: set system properties for selenium dirver
+		System.setProperty("webdriver.gecko.driver", driverPath);
+		
+		// step3: instantiate selenium webdriver
+		WebDriver driver = new FirefoxDriver();
+		
+		// step4: launch browser
+		driver.get(siteUrl);
+		
+		// step5 : Evaluate the Test
+		
+		// step6: Close browser
+		driver.close();
+	}
+}
---
+++ PHASE - 1/com.shivanshu.selenium_project/src/main/java/com/simplilearn/seleniumtest/exwebelement/ExternalIframeTest.java
@@ -0,0 +1,5 @@
+package com.simplilearn.seleniumtest.exwebelement;
+
+public class ExternalIframeTest {
+
+}
