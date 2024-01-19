49cc00926a6bf25a8e9b15cce8e800b0a83b31a2
Shivanshu Mishra <shivanshumishra.email@gmail.com>
2024-01-20 00:20:05 +0530

    Added P3P3

---
+++ PHASE - 3/phase3-restassured-testng-18-01-2024-main/src/test/java/restTesting/DeleteRequestTest.java
@@ -0,0 +1,43 @@
+package restTesting;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class DeleteRequestTest {
+
+	private static final String BASE_URL = "https://reqres.in/api";
+	private static final Logger logger = Logger.getLogger(DeleteRequestTest.class);
+	
+	@DataProvider(name = "deleteUserRestAssured")
+	public Iterator<Object[]> deleteUserRestAssured() {
+		final List<Object[]> deleteData = new ArrayList<Object[]> ();
+		deleteData.add(new Object[] {2});
+		deleteData.add(new Object[] {7});
+		deleteData.add(new Object[] {11});
+		return deleteData.iterator();
+	}
+	
+	
+	@Test(description = "Test for executing DELETE request using rest assured",
+			dataProvider = "deleteUserRestAssured")
+	public void putRequestTest(final int userId) { 
+		logger.info("Start :: Delete user by {userId} test.");
+		
+		logger.info("DELTE : URL "+BASE_URL + "/users/="+userId);
+		// create user post data				
+		RestAssured.given().baseUri(BASE_URL).when()
+		.contentType(ContentType.JSON)
+		.delete("/users/"+userId).then()
+		.assertThat().statusCode(204);
+		
+		logger.info("End :: Delete user test.");
+	}
+}
---
+++ PHASE - 3/phase3-restassured-testng-18-01-2024-main/src/test/java/restTesting/PutRequestTest.java
@@ -0,0 +1,42 @@
+package restTesting;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class PutRequestTest {
+
+	private static final String BASE_URL = "https://reqres.in/api";
+	private static final Logger logger = Logger.getLogger(PutRequestTest.class);
+
+	@Test(description = "Test for executing PUT request using rest assured")
+	public void putRequestTest() {
+		logger.info("Start :: PUT request test using rest assured.");
+
+		String response = null;
+		try {
+			// create user post data
+			int userId = 7;
+			
+			PostData postData = new PostData("Mike SMith", "QA/Tester");
+			logger.info("Request Object:: "+postData);
+			
+			RestAssured.given().baseUri(BASE_URL).when().contentType(ContentType.JSON).body(postData)
+					.put("/users/" + userId).then().assertThat().statusCode(200).and().assertThat()
+					.body("name", equalTo(postData.name)).and().assertThat().body("job", equalTo(postData.job));
+
+			response = RestAssured.given().baseUri(BASE_URL).when().contentType(ContentType.JSON).body(postData)
+					.post("/users").getBody().asString();
+		} catch (Exception e) {
+			logger.error("Exception Object :: " + e.toString());
+			logger.error("End Exception :: " + e.getLocalizedMessage());
+		}
+
+		logger.info("Response :: " + response);
+		logger.info("End :: PUT request test using rest assured.");
+	}
+}
