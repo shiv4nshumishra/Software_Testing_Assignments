d161a5ea7fb4f86ccf644f73dfdf8c0ad0e934e2
Shivanshu Mishra <shivanshumishra.email@gmail.com>
2024-03-01 12:39:44 +0530

    DemoWebShop Project and other Materials added.....

--- demowebshop.tricentis.com/test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"cdd3f3b1-bcc0-472f-a087-0779cc37d009","feature":"To Test DemoWebShop Wishlist Functionality","scenario":"User can add and remove products from the wishlist","start":1709161692729,"group":1,"content":"","tags":"@wishlisttest,","end":1709161726030,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
-});
